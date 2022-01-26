30d6510e01e39538f72a0ac4fba7726b9e864c06
Javier Garcia h <javintcoder@gmail.com>
2022-01-26 13:53:11 +0100

    Basic migrations

--- database/migrations/2022_01_25_020404_create_users_table.php
+++ database/migrations/2022_01_25_020404_create_users_table.php
@@ -16,10 +16,10 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('email')->unique;
             $table->string('password');
-            $table->rememberToken();
+            $table->enum('role', ['Particular','Profesional','Admin']);
+            $table->string('api_token')->nullable()->unique();
             $table->timestamps();
         });
     }
